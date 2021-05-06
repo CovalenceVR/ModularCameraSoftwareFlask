ca4b5b42d4dc8411ecf62f036f7d84ef8818c18c
benjamin <benengel94@gmail.com>
2021-05-06 15:00:10 -0700

    fixed mass delete buttons

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def remove(fileName=None):
     return flask.render_template('cam.html', ip=flask.request.host, status='list')
 
 @app.route('/deleteall', methods=['GET','POST'])
-def remove():
+def removeall():
     Pi.deleteall()
     return flask.render_template('cam.html', ip=flask.request.host, status='list')
 
@@ -60,7 +60,7 @@ def download(filename):
     return flask.send_from_directory(DOWNLOAD_FOLDER, filename=filename, as_attachment=True)
 
 @app.route('/downloadall/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
+def downloadall(filename):
     Pi.downloadall(filename)
     return flask.send_from_directory(DOWNLOAD_FOLDER, filename=filename, as_attachment=True)
 
