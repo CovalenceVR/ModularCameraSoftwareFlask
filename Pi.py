efd1575007fb0ef6cd025d6a9abbafdf55d1408d
benjamin <benengel94@gmail.com>
2021-05-06 15:35:54 -0700

    Mass download?

--- Pi.py
+++ Pi.py
@@ -14,6 +14,10 @@ def deleteall():
     for f in os.listdir("projects"):
         os.remove(os.path.join("projects", f))
 
+def downloadall(fileName):
+    os.system("zip -r " + fileName + ".zip projects")
+    os.system("mv " + fileName + ".zip projects")
+
 def still(fileName):
     os.system("raspistill -n -o projects/" + fileName + ".png")
 
